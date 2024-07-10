3b70bd3a10bf58a2c4a2f139588547cfa8a621b6
Nada <nadafarrag619@gmail.com>
2024-07-10 22:56:13 +0300

    file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,11 @@
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
-CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email CHAR(255) NOT NULL UNIQUE, name CHAR(255));
+-- A table called user with attributes 
+-- id integer, not null, auto incr and primary key
+-- email string with 255 character, not null, and unique
+-- name string 255 character
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
